316f561358a30609dc8d1afea2db2f36868957ac
Brian Walter <brianwalter1@mail.weber.edu>
2017-09-30 13:49:43 -0600

    Initial commit

---
+++ Galaxy_Hw3_Task1.cpp
@@ -0,0 +1,19 @@
+// File: Galaxy_Hw3_Task2
+// Created by Brian Walter on 9/30/2017 for CS1410.
+// Copyright (c) 2017 WSU
+#include <iostream>
+
+using namespace std;
+
+// Constants
+
+// Prototypes
+
+// Main Program Program
+int main(void) {
+
+
+    return 0;
+
+}
+// Function Definitions
