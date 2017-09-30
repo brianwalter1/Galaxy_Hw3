d3d2dde11b6768e6462c915e3618777b9c85385c
Brian Walter <brianwalter1@mail.weber.edu>
2017-09-30 14:06:59 -0600

    Wrote class functions, TODO write main program

--- Galaxy_Hw3_Task1.cpp
+++ Galaxy_Hw3_Task1.cpp
@@ -27,3 +27,24 @@ int main(void) {
 
 }
 // Function Definitions
+void date::getDate() {
+    //Variables to store date values
+    int a,b,c;
+    char tempchar;
+
+    //Prompt the user for the date and store it
+    cout << "Enter a date (format 12/31/1999): ";
+    cin >> a >> tempchar >> b >> tempchar >> c;
+
+    //Assign the date to appropriate class variables
+    month = a;
+    day = b;
+    year = c;
+
+    //Return control of the program
+    return;
+}
+
+void date::showDate() {
+    cout << month << "/" << day << "/" << year << endl;
+}
